ba4df1c4aa9f82f215af41ccf217de6b8eeb999a
LENOVO-PC\LENOVO <evanmeng1226@gmail.com>
2020-03-01 17:07:30 +1300

    no message

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Link} from 'gatsby';
 import Header from '../components/Header';
-import { Helmet } from 'react-helmet';
 
 export default ()=>(
     <div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'gatsby';
+import { Helmet } from 'react-helmet';
 
 export default () => {
 
